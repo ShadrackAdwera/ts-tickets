5236b700e8bf27fc5d2c630e4c2cbad9b0ca3b0e
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-24 12:08:15 +0300

    create auth routes

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,8 +1,12 @@
 import express, { NextFunction, Request, Response } from 'express';
 
+import authRoutes from './routes/auth-routes';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.use((_req: Request, _res: Response, _next: NextFunction) => {
     throw new Error('Cannot find method / route');
