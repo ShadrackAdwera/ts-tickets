b918191094bedffb2f312b5b61f1bca6551db4d9
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-24 16:32:55 +0300

    set up connection to mongoose

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,4 +1,5 @@
 import express, { NextFunction, Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 import authRoutes from './routes/auth-routes';
 
@@ -17,6 +18,10 @@ app.use((error: Error ,_req: Request, res: Response, next: NextFunction) => {
         return next(error);
     }
     res.status(500).json({message: error.message || 'An error occured, try again'});
-})
+});
 
-app.listen(5000, ()=>console.log('Auth service listening on PORT: 5000!'));
+mongoose.connect(<string>process.env.MONGO_URI)
+.then(_=>{
+    app.listen(5000);
+    console.log('Auth service listening on PORT: 5000!')
+}).catch(error=>console.log(error));
