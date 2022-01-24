e6e487e51efb58c5f1ddf79fe781809773720c98
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-24 16:32:21 +0300

    complete reset password controller

--- auth/src/controllers/auth-controllers.ts
+++ auth/src/controllers/auth-controllers.ts
@@ -1,15 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 import { validationResult } from 'express-validator';
+import { Document } from 'mongoose';
+import brypto from 'crypto';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
 import User from '../models/User';
 import HttpError from '../models/HttpError';
 
-interface UserProps {
-    _id: number,
+interface UserProps extends Document {
     email: string;
     password: string;
+    resetToken: any;
+    tokenExpirationDate: any;
 }
 
 const signUp = async(req: Request, res: Response, next: NextFunction) => {
@@ -39,7 +42,7 @@ const signUp = async(req: Request, res: Response, next: NextFunction) => {
         return next(new HttpError('An error occured, try again',500));
     }
 
-    const user = new User<{
+    const user: UserProps = new User<{
         email: string, password: string, resetToken: any, tokenExpirationDate: any  
     }>({
         email, password, resetToken: null, tokenExpirationDate: undefined
@@ -52,7 +55,7 @@ const signUp = async(req: Request, res: Response, next: NextFunction) => {
     }
 
     try {
-        token = jwt.sign({ id: user._id.toString(), email }, 'jsonsupersecretkey',  { expiresIn: '1h' });
+        token = jwt.sign({ id: user._id.toString() , email }, 'jsonsupersecretkey',  { expiresIn: '1h' });
     } catch (error) {
         return next(new HttpError('An error occured, login to continue',500));
     }
@@ -98,37 +101,72 @@ const login = async(req: Request, res: Response, next: NextFunction) => {
     res.status(200).json({message: 'Login successful', user: { id: foundUser._id.toString(), email, token }});
 }
 
-const requestResetPassword = (req: Request, res: Response, next: NextFunction) => {
+const requestResetPassword = async(req: Request, res: Response, next: NextFunction) => {
     const { email } = req.body as { email: string };
+    let foundUser: UserProps;
 
     const error = validationResult(req);
     if(!error.isEmpty()) {
         return next(new HttpError('Invalid inputs', 422));
     }
 
-    console.log(email);
+    try {
+        foundUser = await User.findOne({email}).exec();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again',500));
+    }
 
-    // check if email exists in DB
-    // create a reset token, modify user to add reset token + expiry date for token
-    // send reset link via email
+    if(!foundUser) {
+        return next(new HttpError('This account does not exist, sign up in instead', 403));
+    }
+
+    const authToken = brypto.randomBytes(64).toString('hex');
+    const tokenExpiration = Date.now() + 3600000;
+    foundUser.resetToken = authToken;
+    foundUser.tokenExpirationDate = new Date(tokenExpiration);
+    // send reset link via email--- https://your-site/${resetToken}/reset-password
+
+    try {
+        await foundUser.save();
+    } catch (error) {
+        
+    }
 
     res.status(200).json({message: 'Reset link sent via email'});
 }
 
-const resetPassword = (req: Request, res: Response, next: NextFunction) => {
+const resetPassword = async(req: Request, res: Response, next: NextFunction) => {
     const { password } = req.body as { email: string, password: string };
     const resetToken  = <string>req.params?.resetToken;
+    let foundUser: UserProps;
+    let newPassword: string;
 
     const error = validationResult(req);
     if(!error.isEmpty()) {
         return next(new HttpError('Invalid inputs', 422));
     }
-    console.log(resetToken);
 
-    // find account using reset token and expiry date
-    // hash password
-    // patch the account to use this password, set token to null and expiry date to undefined
+    try {
+        foundUser = await User.findOne({resetToken, tokenExpirationDate: { $gt: Date.now() }});
+    } catch (error) {
+        return next(new HttpError('An error occured, try again',500));
+    }
 
+    try {
+        newPassword = await bcrypt.hash(password, 12);
+    } catch (error) {
+        return next(new HttpError('An error occured, try again',500));
+    }
+
+    foundUser.password = newPassword;
+    foundUser.resetToken = null;
+    foundUser.tokenExpirationDate = undefined;
+    
+    try {
+        await foundUser.save();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again',500));
+    }
     res.status(200).json({message: 'Password reset successful'});
 }
 
