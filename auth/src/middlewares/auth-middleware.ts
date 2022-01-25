06b803369c2dd183a863ed40d86d93755faea62e
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-25 15:35:40 +0300

    fix auth middleware bug

--- auth/src/middlewares/auth-middleware.ts
+++ auth/src/middlewares/auth-middleware.ts
@@ -3,7 +3,7 @@ import jwt , { JwtPayload } from 'jsonwebtoken';
 import HttpError from '../models/HttpError';
 
 interface JsonWebTokenProps extends JwtPayload {
-    id: string;
+    userId: string;
     email: string;
 }
 
@@ -17,21 +17,21 @@ declare global {
     }
 }
 
-const checkAuth = (req: Request, res: Response, next: NextFunction) => {
+const checkAuth = (req: Request, _res: Response, next: NextFunction) => {
     if(req.method==='OPTIONS') {
         return next();
     }
 
     let decodedToken: JsonWebTokenProps;
     const token = req.headers.authorization?.split(' ')[1];
-   
+
     if(!token) {
         return next(new HttpError('Auth failed', 401));
     }
 
     try {
         decodedToken = jwt.verify(token, process.env.JWT_KEY!) as JsonWebTokenProps;
-        req.user = { id: decodedToken?.id, email: decodedToken?.email };
+        req.user = { userId: decodedToken?.id, email: decodedToken?.email };
         next();
     } catch (error) {
         return next(new HttpError('Auth failed', 401));
--- auth/src/routes/auth-routes.ts
+++ auth/src/routes/auth-routes.ts
@@ -1,32 +1,45 @@
-import { body } from 'express-validator';
-import express from 'express';
+import { body } from "express-validator";
+import express from "express";
 
-import AuthControllers from '../controllers/auth-controllers';
+import AuthControllers from "../controllers/auth-controllers";
+import checkAuth from "../middlewares/auth-middleware";
 
-const { login, signUp, requestResetPassword, resetPassword } = AuthControllers;
+const { getCurrentUser, login, signUp, requestResetPassword, resetPassword } =
+  AuthControllers;
 
 const router = express.Router();
 
-router.post('/sign-up', [
-    body('email').trim().normalizeEmail().isEmail(),
-    body('password').trim().isLength({min: 6})
-], signUp);
+router.post(
+  "/sign-up",
+  [
+    body("email").trim().normalizeEmail().isEmail(),
+    body("password").trim().isLength({ min: 6 }),
+  ],
+  signUp
+);
 
-router.post('/login', [
-    body('email').trim().normalizeEmail().isEmail(),
-    body('password').trim().isLength({min: 6})
-], login);
+router.post(
+  "/login",
+  [
+    body("email").trim().normalizeEmail().isEmail(),
+    body("password").trim().isLength({ min: 6 }),
+  ],
+  login
+);
 
-router.post('/request-reset-password', [
-    body('email').trim().normalizeEmail().isEmail()
-], requestResetPassword);
+router.post(
+  "/request-reset-password",
+  [body("email").trim().normalizeEmail().isEmail()],
+  requestResetPassword
+);
 
-router.post('/reset-password/:resetToken', [
-    body('password').trim().isLength({min: 6})
-], resetPassword);
+router.post(
+  "/reset-password/:resetToken",
+  [body("password").trim().isLength({ min: 6 })],
+  resetPassword
+);
+
+router.use(checkAuth);
+router.get("/current-user", getCurrentUser);
 
 export default router;
-
-
-
-
