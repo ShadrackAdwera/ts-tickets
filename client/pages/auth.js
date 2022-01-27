d2fc63a20031a2a457ff956185014196de5addfe
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-27 22:29:23 +0300

    code refactor on auth.js

--- client/pages/auth.js
+++ client/pages/auth.js
@@ -1,33 +1,49 @@
-import { useRouter } from 'next/router';
-import React, { useCallback } from 'react';
-import { CircularProgress } from '@mui/material';
+import { useRouter } from "next/router";
+import React, { useCallback } from "react";
+import { CircularProgress } from "@mui/material";
 
-import useHttp from '../hooks/http-hook';
-import AuthForm from '../components/auth/auth-form';
+import useHttp from "../hooks/http-hook";
+import AuthForm from "../components/auth/auth-form";
 
 const Auth = () => {
   const router = useRouter();
   const { isLoading, sendRequest } = useHttp();
 
-  const loginHandler = useCallback(async(email, password, url) => {
-    const response = await sendRequest(url, 'POST', JSON.stringify({email, password}), {
-      'Content-Type': 'application/json'
-    }); 
-    if(response && response.user) {
-      router.replace('/');
+  const loginHandler = useCallback(async (email, password, url) => {
+    const response = await sendRequest(
+      url,
+      "POST",
+      JSON.stringify({ email, password }),
+      {
+        "Content-Type": "application/json",
+      }
+    );
+    if (response && response.user) {
+      router.replace("/");
     }
-  },[]);
+  }, []);
 
-  const signUpHandler = useCallback(async(email, password, url) => {
-    const response = await sendRequest(url, 'POST', JSON.stringify({email, password}), {
-      'Content-Type': 'application/json'
-    });
-    if(response && response.user) {
-      router.replace('/');
+  const signUpHandler = useCallback(async (email, password, url) => {
+    const response = await sendRequest(
+      url,
+      "POST",
+      JSON.stringify({ email, password }),
+      {
+        "Content-Type": "application/json",
+      }
+    );
+    if (response && response.user) {
+      router.replace("/");
     }
-  },[]);
+  }, []);
 
-  return <AuthForm onLogin={loginHandler} onSignUp={signUpHandler} isLoading={isLoading}/>
+  return (
+    <AuthForm
+      onLogin={loginHandler}
+      onSignUp={signUpHandler}
+      isLoading={isLoading}
+    />
+  );
 };
 
 export default Auth;
