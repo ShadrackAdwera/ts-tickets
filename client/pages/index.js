4c8d8ac76f1fee6cf0981a4ca3c50a4dc1f3dee4
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-28 14:54:33 +0300

    persist current logged in user session

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,8 +1,22 @@
+import { getSession, signOut } from 'next-auth/client';
+import { Button } from '@mui/material';
 import Head from 'next/head'
 import Layout from '../components/layout/layout'
-import styles from '../styles/Home.module.css'
 
-export default function Home() {
+import styles from '../styles/Home.module.css';
+
+const getAuth = (stringifiedAuth) => {
+  //pass session.user.email
+  return JSON.parse(stringifiedAuth);
+}
+
+export default function Home(props) {
+  const { session } = props; 
+  const authInfo = getAuth(session.user.email); 
+  const logoutHandler = () => {
+    signOut();
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +26,29 @@ export default function Home() {
       </Head>
     <Layout>
       <p>Welcome to k8s tickets</p>
+      <p className={styles.description}>
+          {`Your are ${authInfo && authInfo.token? 'Logged in': 'not logged in.'}`}
+        </p>
+        <div style={{margin: '1rem 0'}}></div>
+        <Button variant="text" onClick={logoutHandler}>Logout</Button>
     </Layout>
     </div>
   )
+}
+
+export async function getServerSideProps(context){
+  const session = await getSession({req: context.req});
+  if(!session) {
+    return {
+      redirect: {
+        destination: '/auth/login',
+        permanent: false
+      }
+    }
+  }
+  return {
+    props: {
+      session
+    }
+  }
 }
