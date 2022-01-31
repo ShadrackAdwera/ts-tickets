c5d4dd21dfaef75b30a96fbfcfd0bc8a1df3ba67
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-31 19:45:44 +0300

    create tickets schema

---
+++ tickets/src/models/Ticket.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+import { Schema } from 'mongoose';
+
+const ticketsSchema = new Schema({
+    title: { type: String, required: true },
+    userId: { type: Schema.Types.ObjectId, required: true }
+}, { timestamps: true, toJSON: { getters: true } });
+
+export default mongoose.model('ticket', ticketsSchema);
