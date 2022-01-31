a34faa37d1b79689d7e31849860e8c55554ae14f
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-31 20:14:51 +0300

    create ticket routes

--- tickets/src/routes/ticket-routes.ts
+++ tickets/src/routes/ticket-routes.ts
@@ -1,1 +1,27 @@
-//ticket routes
+import { Router } from 'express';
+import { body } from 'express-validator';
+import { checkAuth } from '@adwesh/common';
+
+import ticketControllers from '../controllers/ticket-controllers';
+
+const router: Router = Router();
+
+const { getTickets, findTicketById, createTicket, updateTicket } = ticketControllers;
+
+router.get('/', getTickets);
+router.get('/:ticketId', findTicketById);
+
+router.use(checkAuth);
+
+router.post('/new', [
+    body('title').trim().isLength({min: 6}),
+    body('price').trim().isLength({min: 2})
+], createTicket);
+
+router.patch('/:ticketId', [
+    body('title').trim().isLength({min: 6}),
+    body('price').trim().isLength({min: 2})
+], updateTicket);
+
+export default router;
+
