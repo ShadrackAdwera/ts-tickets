c3861051624fa9d845463150fc2f7762e7287f20
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-31 19:54:52 +0300

    create get ticket controller

--- tickets/src/controllers/ticket-controllers.ts
+++ tickets/src/controllers/ticket-controllers.ts
@@ -1,1 +1,44 @@
-//tickets controllers
+import { HttpError } from '@adwesh/common';
+import { validationResult, ValidationError, Result } from 'express-validator';
+import { Request, Response, NextFunction } from 'express';
+import { Document } from 'mongoose';
+
+import Ticket from '../models/Ticket';
+
+interface TicketDoc extends Document {
+    title: string;
+    userId: string
+}
+
+
+const getTickets = async(req: Request, res: Response, next: NextFunction) => {
+    let tickets: TicketDoc[];
+
+    try {
+        tickets = await Ticket.find().exec();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+    res.status(200).json({tickets});
+}
+
+const findTicketById = async(req: Request, res: Response, next: NextFunction) => {
+    const ticketId = req.params.ticketId;
+    let ticket: TicketDoc;
+
+    if(!ticketId) {
+        return next(new HttpError('Invalid ticket', 400));
+    }
+
+    try {
+        ticket = await Ticket.findById(ticketId).exec();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+    if(!ticket) {
+        return next(new HttpError('This ticket does not exist!', 404));
+    }
+    res.status(200).json({ticket});
+}
+
+
