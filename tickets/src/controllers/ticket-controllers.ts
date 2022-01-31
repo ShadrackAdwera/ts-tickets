de592c0c28198c336e3d64fed151a8ea5ffa54ad
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-31 20:01:59 +0300

    create post ticket controller

--- tickets/src/controllers/ticket-controllers.ts
+++ tickets/src/controllers/ticket-controllers.ts
@@ -11,6 +11,7 @@ interface TicketDoc extends Document {
 }
 
 
+
 const getTickets = async(req: Request, res: Response, next: NextFunction) => {
     let tickets: TicketDoc[];
 
@@ -39,6 +40,27 @@ const findTicketById = async(req: Request, res: Response, next: NextFunction) =>
         return next(new HttpError('This ticket does not exist!', 404));
     }
     res.status(200).json({ticket});
+}
+
+const createTicket = async(req: Request, res: Response, next: NextFunction) => {
+    const error: Result<ValidationError> = validationResult(req);
+    if(!error.isEmpty()) {
+        return next(new HttpError('Invalid ticket inputs', 422));
+    }
+    const { title } = req.body;
+    const id = req.user?.userId;
+
+    const newTicket: TicketDoc = new Ticket({
+        title, userId: id
+    });
+
+    try {
+        await newTicket.save();
+    } catch (error) {
+        return next(new HttpError('An error occured, try again', 500));
+    }
+
+    res.status(201).json({message: 'The ticket has been successfully created' ,ticket: newTicket})
 }
 
 
