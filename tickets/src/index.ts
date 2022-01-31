147b8579af3ed4f46d6a0b24ba81d5d3ca9f1d77
ShadrackAdwera <adweshshaddie@gmail.com>
2022-01-31 20:20:00 +0300

    configure connection to mongo db

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,1 +1,23 @@
-//mongoose stuff
+import mongoose from 'mongoose';
+import app from './app';
+
+const start = async() => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('JWT must be defined');
+    }
+
+    if(!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI must be defined');
+    }
+
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        app.listen(5001);
+    console.log('Tickets service listening on PORT: 5001!')
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+start();
