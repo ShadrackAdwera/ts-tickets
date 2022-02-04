b4bc8b4e2418b341eebc88f1f75c581f4085c7d5
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-04 12:46:10 +0300

    set up connection to mongodb

---
+++ orders/src/index.ts
@@ -0,0 +1,47 @@
+import mongoose from 'mongoose';
+
+import { natsWraper } from './nats-wrapper';
+import app from './app';
+
+const start = async() => {
+    if(!process.env.JWT_KEY) {
+        throw new Error('JWT must be defined');
+    }
+
+    if(!process.env.MONGO_URI) {
+        throw new Error('MONGO_URI must be defined');
+    }
+
+    if(!process.env.NATS_CLUSTER_ID) {
+        throw new Error('NATS_CLUSTER_ID must be defined');
+    }
+
+    if(!process.env.NATS_CLIENT_ID) {
+        throw new Error('NATS_CLIENT_ID must be defined');
+    }
+
+    if(!process.env.NATS_URI) {
+        throw new Error('NATS_URI must be defined');
+    }
+
+    try {
+        await natsWraper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URI);
+
+        natsWraper.client.on('close', () => {
+            console.log('NATS shutting down . . .');
+            process.exit();
+        });
+
+        process.on('SIGINT', () => natsWraper.client.close());
+        process.on('SIGTERM', () => natsWraper.client.close());
+
+        await mongoose.connect(process.env.MONGO_URI);
+        app.listen(5001);
+    console.log('Orders service listening on PORT: 5001!')
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+start();
