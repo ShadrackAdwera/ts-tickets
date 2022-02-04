06220ab792d66d0cce6c26539000836ee1383c81
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-04 12:45:05 +0300

    handle route errors and CORS

---
+++ orders/src/app.ts
@@ -0,0 +1,29 @@
+import { HttpError } from '@adwesh/common';
+import express, { NextFunction, Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+//CORS
+app.use((_req: Request, res: Response, next: NextFunction) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods','OPTIONS, PUT, PATCH, POST, DELETE, GET');
+    next();
+  });
+
+//use routes
+
+app.use((_req: Request, _res: Response, _next: NextFunction) => {
+    throw new HttpError('This method / route does not exist', 404);
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction)=> {
+    if(res.headersSent) {
+        return next(error);
+    }
+    res.status(500).json({message: error.message || 'An error occured try again' });
+});
+
+export default app;
--- orders/src/nats-wrapper.ts
+++ orders/src/nats-wrapper.ts
@@ -14,7 +14,7 @@ class NatsWrapper {
         this._client = nats.connect(clusterId, clientId, { url });
         return new Promise<void>((resolve, reject)=>{
             this.client.on('connect', ()=>{
-                console.log('Connected to NATS');
+                console.log('Order Service Connected to NATS');
                 resolve();
             });
             this.client.on('error', (err)=>{
