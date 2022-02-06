5793752c70c25a8eb9d4eef88fbae92b0ede3209
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-06 15:58:55 +0300

    delay a job using expiresAt time from the received event

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -3,15 +3,17 @@ import { Message } from 'node-nats-streaming';
 import { expirationQueue } from '../../queues/expiration-queue';
 
 
-const ORDERS_QUEUE_GROUP = 'expiration-service'
+const ORDERS_QUEUE_GROUP = 'expiration-service';
+
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     subject: Subjects.OrderCreated = Subjects.OrderCreated;
     queueGroupName: string = ORDERS_QUEUE_GROUP;
     async onMessage(data: OrderCreatedEvent['data'], msg: Message): Promise<void> {
+        const delayTime = new Date(data.expiresAt).getTime() - new Date().getTime();
         await expirationQueue.add({
             orderId: data.id
-        });
+        }, { delay: delayTime});
 
         msg.ack();
     }
