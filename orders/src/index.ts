9602475d8dac182f9aedd34b548c74636620dbd7
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-06 16:47:54 +0300

    wire the orders service to listen to expiration events

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,7 @@ import { natsWraper } from './nats-wrapper';
 import { TicketCreatedListener } from '../events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from '../events/listeners/ticket-updated-listener';
 import app from './app';
+import { ExpirationCompleteListener } from '../events/listeners/expiration-complete-listener';
 
 const start = async() => {
     if(!process.env.JWT_KEY) {
@@ -39,6 +40,7 @@ const start = async() => {
 
         new TicketCreatedListener(natsWraper.client).listen();
         new TicketUpdatedListener(natsWraper.client).listen();
+        new ExpirationCompleteListener(natsWraper.client).listen();
 
         await mongoose.connect(process.env.MONGO_URI);
         app.listen(5002);
