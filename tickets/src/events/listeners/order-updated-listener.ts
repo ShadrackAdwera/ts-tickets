90227ac6aceda5c4550a81016cafb62c3bc48edf
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-06 14:08:36 +0300

    publish ticket updated event on order cancel

--- tickets/src/events/listeners/order-updated-listener.ts
+++ tickets/src/events/listeners/order-updated-listener.ts
@@ -1,15 +1,17 @@
 import { Document } from 'mongoose';
-import { Subjects, Listener, OrderCancelledEvent, OrderStatus } from '@adwesh/common';
+import { Subjects, Listener, OrderCancelledEvent } from '@adwesh/common';
 import { Message } from 'node-nats-streaming';
 
 import { TICKETS_QUEUE_GROUP } from './constants';
 import Ticket from '../../models/Ticket';
+import { TicketUpdatedPublisher } from '../publishers/ticket-updated-publisher';
 
 interface TicketDoc extends Document {
     title: string;
     price: number;
     userId: string;
     orderId?: string;
+    version: number;
 }
 
 export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
@@ -17,16 +19,21 @@ export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
     queueGroupName: string = TICKETS_QUEUE_GROUP;
     async onMessage(data: OrderCancelledEvent['data'], msg: Message) {
         let foundTicket: TicketDoc;
-        console.log(data);
         foundTicket = await Ticket.findById(data.ticket.id).exec();
 
         if(!foundTicket) {
             throw new Error('This ticket does not exist!');
         }
-        console.log(foundTicket);
         foundTicket.orderId = undefined;
         try {
             await foundTicket.save();
+            await new TicketUpdatedPublisher(this.client).publish({
+                id: foundTicket.id.toString(), 
+                price: foundTicket.price, 
+                title: foundTicket.title, 
+                userId: foundTicket.userId, 
+                version: foundTicket.version
+            })
             msg.ack();
         } catch (error) {
             throw new Error('An error occured, try again');
