133b382569d3dd4a451f9708bee4366679cc6bf3
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-08 07:46:24 +0300

    set up the order cancelled listener in the payment service

---
+++ payments/src/events/listeners/order-cancelled-listener.ts
@@ -0,0 +1,29 @@
+import { Subjects, Listener, OrderCancelledEvent, OrderStatus } from '@adwesh/common';
+import { Message } from 'node-nats-streaming';
+
+import { Order } from '../../models/Orders';
+
+const PAYMENT_QUEUE_GROUP = 'payments-service';
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+    subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
+    queueGroupName: string = PAYMENT_QUEUE_GROUP;
+    
+    async onMessage(data: OrderCancelledEvent['data'], msg: Message): Promise<void> {
+        const { id } = data;
+        let foundOrder = await Order.find({orderId: id}).exec();
+
+        if(!foundOrder || foundOrder.length===0) {
+            throw new Error('This order does not exist');
+        }
+
+        foundOrder[0].status = OrderStatus.Cancelled;
+
+        try {
+            await foundOrder[0].save();
+            msg.ack();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
