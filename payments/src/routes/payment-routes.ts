c811520f75285f01b43e277d89f2957691301e83
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-08 12:59:35 +0300

    add create payments route

---
+++ payments/src/routes/payment-routes.ts
@@ -0,0 +1,18 @@
+import { body } from 'express-validator';
+import { checkAuth } from '@adwesh/common';
+import express from 'express';
+
+import { createPayment } from '../controllers/payment-controllers';
+
+const router = express.Router();
+
+router.use(checkAuth);
+
+router.post('/', [
+    body('orderId').not().isEmpty(),
+    body('token').not().isEmpty()
+], createPayment);
+
+export default router;
+
+
