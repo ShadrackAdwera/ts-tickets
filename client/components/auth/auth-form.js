34ab67fa21164565d3f531631a0105ed63658f1b
ShadrackAdwera <adweshshaddie@gmail.com>
2022-02-09 16:20:51 +0300

    code refactor on the auth component

--- client/components/auth/auth-form.js
+++ client/components/auth/auth-form.js
@@ -1,26 +1,26 @@
-import { TextField, Button, CircularProgress } from '@mui/material';
+import { TextField, Button, CircularProgress } from "@mui/material";
 import { useState, useReducer } from "react";
 
-import Layout from '../layout/layout';
+import Layout from "../layout/layout";
 import classes from "./auth-form.module.css";
 
 const initialState = {
-  email: '',
-  password: ''
-}
+  email: "",
+  password: "",
+};
 
 const reducer = (state, action) => {
-  switch(action.type) {
-    case 'INPUT_EMAIL':
-      return {...state, email: action.value}
-    case 'INPUT_PASSWORD':
-      return {...state, password: action.value}
+  switch (action.type) {
+    case "INPUT_EMAIL":
+      return { ...state, email: action.value };
+    case "INPUT_PASSWORD":
+      return { ...state, password: action.value };
     default:
       return state;
   }
-}
+};
 
-function AuthForm({onLogin, onSignUp, isLoading, session, signOut}) {
+function AuthForm({ onLogin, onSignUp, isLoading, session, signOut }) {
   const [isLogin, setIsLogin] = useState(true);
   const [inputState, dispatch] = useReducer(reducer, initialState);
 
@@ -31,45 +31,61 @@ function AuthForm({onLogin, onSignUp, isLoading, session, signOut}) {
   const submitHandler = (e) => {
     e.preventDefault();
     const { email, password } = inputState;
-    let url = 'http://192.168.49.2:31925/api/auth';
-    if(isLogin) {
+    let url = "http://192.168.49.2:31925/api/auth";
+    if (isLogin) {
       onLogin(email, password);
     } else {
       onSignUp(email, password, `${url}/sign-up`);
     }
-  }
+  };
 
   return (
     <Layout signOut={signOut} session={session}>
-    <section className={classes.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form onSubmit={submitHandler}>
-         <div className={classes.control}>
-            <TextField label='Email' 
-            type='email' 
-            fullWidth 
-            variant='standard' 
-            value={inputState.email} 
-            onChange={e=>dispatch({type: 'INPUT_EMAIL', value: e.target.value})}/>
-         </div>
-         <div className={classes.control}>
-            <TextField label='Password' 
-            type='password' 
-            fullWidth 
-            variant='standard' 
-            value={inputState.password} 
-            onChange={e=>dispatch({type: 'INPUT_PASSWORD', value: e.target.value})}/>
-         </div>
-        <div className={classes.actions}>
-          {isLoading? <CircularProgress /> : <Button type='submit'>{isLogin ? "Login" : "Create Account"}</Button>}
-          <Button type="button"
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}>
-             {isLogin ? "Create new account" : "Login with existing account"}
-          </Button>
-        </div>
-      </form>
-    </section>
+      <section className={classes.auth}>
+        <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+        <form onSubmit={submitHandler}>
+          <div className={classes.control}>
+            <TextField
+              label="Email"
+              type="email"
+              fullWidth
+              variant="standard"
+              value={inputState.email}
+              onChange={(e) =>
+                dispatch({ type: "INPUT_EMAIL", value: e.target.value })
+              }
+            />
+          </div>
+          <div className={classes.control}>
+            <TextField
+              label="Password"
+              type="password"
+              fullWidth
+              variant="standard"
+              value={inputState.password}
+              onChange={(e) =>
+                dispatch({ type: "INPUT_PASSWORD", value: e.target.value })
+              }
+            />
+          </div>
+          <div className={classes.actions}>
+            {isLoading ? (
+              <CircularProgress />
+            ) : (
+              <Button type="submit">
+                {isLogin ? "Login" : "Create Account"}
+              </Button>
+            )}
+            <Button
+              type="button"
+              className={classes.toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? "Create new account" : "Login with existing account"}
+            </Button>
+          </div>
+        </form>
+      </section>
     </Layout>
   );
 }
